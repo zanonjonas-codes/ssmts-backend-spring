eaa40032cd1562010cb3caa15b88b0a2918f2a02
Jonas Zanon <zanonjonas.codes@gmail.com>
2023-08-22 12:41:30 -0300

    inital commit2

---
+++ src/main/java/com/zanonjonascodes/ssmtsbackendspring/SsmtsBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zanonjonascodes.ssmtsbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmtsBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmtsBackendSpringApplication.class, args);
+	}
+
+}
