0d2a4542964f6b7d43ebc31128af670f54cbe211
Jonas Zanon <zanonjonas.codes@gmail.com>
2023-08-27 01:37:55 -0300

    added user with mustache

---
+++ src/main/java/com/zanonjonascodes/ssmts/user/UserRequestModel.java
@@ -0,0 +1,18 @@
+package com.zanonjonascodes.ssmts.user;
+
+import com.zanonjonascodes.ssmts.core.rest.crud.RequestModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestModel implements RequestModel {
+
+  protected String id;
+
+}
