2306f203587ef7db8b4f417f4415803d15fe4d97
Jonas Zanon <zanonjonas.codes@gmail.com>
2023-09-01 16:30:45 -0300

    fix import

--- src/main/java/com/zanonjonascodes/ssmts/auth/AuthService.java
+++ src/main/java/com/zanonjonascodes/ssmts/auth/AuthService.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
